ca2bae478fe3d3bcb2a6b77ddbd57389e6448640
Tony Ingle <ingleanthonyco@gmail.com>
2022-05-21 14:41:40 -0600

    finishing up mobile nav styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,11 +4,13 @@ import Logo from '../../images/logo.svg';
 
 const Nav = () => {
 	const [activeNav, setActiveNav] = useState(false);
+	const [activeNavLinksContainer, setActiveNavLinksContainer] = useState(false);
 	const [activeNavLinks, setActiveNavLinks] = useState(false);
 	const [activeNavBg, setActiveNavBg] = useState(false);
 	const [activeNavBgAnimation, setActiveNavBgAnimation] = useState(false);
 	const openMenu = () => {
 		setActiveNav(!activeNav);
+		setActiveNavLinksContainer(!activeNavLinksContainer);
 		setActiveNavLinks(!activeNavLinks);
 		setActiveNavBg(!activeNavBg);
 		setActiveNavBgAnimation(!activeNavBgAnimation);
@@ -30,33 +32,39 @@ const Nav = () => {
 					<div className='hamburger-nav-bars bar-two'></div>
 					<div className='hamburger-nav-bars bar-three'></div>
 				</div>
-				<ul className={`nav-links ${activeNavLinks ? 'active-nav-links' : ''}`}>
-					<li>
-						<a className='single-nav-link' href='/'>
-							Home
-						</a>
-					</li>
-					<li>
-						<a className='single-nav-link' href='/'>
-							About
-						</a>
-					</li>
-					<li>
-						<a className='single-nav-link' href='/'>
-							Contact
-						</a>
-					</li>
-					<li>
-						<a className='single-nav-link' href='/'>
-							Blog
-						</a>
-					</li>
-					<li>
-						<a className='single-nav-link' href='/'>
-							Careers
-						</a>
-					</li>
-				</ul>
+				<div
+					className={`nav-links-container ${
+						activeNav ? 'active-nav-links-container' : ''
+					} ${activeNavBgAnimation ? 'fade-in' : ''}`}>
+					<ul
+						className={`nav-links ${activeNavLinks ? 'active-nav-links' : ''}`}>
+						<li>
+							<a className='single-nav-link' href='/'>
+								Home
+							</a>
+						</li>
+						<li>
+							<a className='single-nav-link' href='/'>
+								About
+							</a>
+						</li>
+						<li>
+							<a className='single-nav-link' href='/'>
+								Contact
+							</a>
+						</li>
+						<li>
+							<a className='single-nav-link' href='/'>
+								Blog
+							</a>
+						</li>
+						<li>
+							<a className='single-nav-link' href='/'>
+								Careers
+							</a>
+						</li>
+					</ul>
+				</div>
 				<button className='nav-btn' type='button'>
 					Request Invite
 				</button>
