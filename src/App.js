0a4285c3baea78e50a758d11d09aba7f7a279572
Tony Ingle <ingleanthonyco@gmail.com>
2022-05-25 14:53:20 -0600

    fixed hero overlay

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Nav from './components/Nav/Nav';
 import Hero from './components/Hero/Hero';
 import Features from './components/Features/Features';
 import Articles from './components/Articles/Articles';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
 			<Hero />
 			<Features />
 			<Articles />
+			<Footer />
 		</div>
 	);
 };
